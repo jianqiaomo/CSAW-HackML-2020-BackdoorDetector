be4528931ed94b68c747af0e46589ec695850c7e
wenjiezhu <wz2140@nyu.edu>
2021-12-20 23:05:32 -0500

    zwj commit to fine prune

---
+++ eval_fine_prune/eval_fine_prune_anonymous_1.py
@@ -0,0 +1,13 @@
+import fine_prune
+import sys
+
+if __name__ == '__main__':
+    
+    model_path = 'models_G/anonymous1_repaired_net.h5'
+    img_path = str(sys.argv[1])
+    
+    
+    
+    strip.eval(model_path, img_path)
+    
+    pass
